558c8d2ebdd4a96ab8977a7002a4aef12b4bc277
Ben O'Neill <ben@oneill.sh>
2025-05-12 13:27:32 -0400

    Restructure and add PDF conversion option

--- config.mk
+++ config.mk
@@ -3,7 +3,7 @@ PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 
 CPPFLAGS = 
-CFLAGS   = -std=c99 -pedantic -Wall -O2 $(CPPFLAGS)
+CFLAGS   = -Wall -O2 $(CPPFLAGS)
 LDFLAGS  = -s
 
 # compiler and linker
