bb3de21d0f6e171468fc74abe4738fdbd18ccc2b
Ben O'Neill <ben@oneill.sh>
2025-06-24 16:57:19 -0400

    Fix arg parsing

--- src/md2roff.c
+++ src/md2roff.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]) {
-    char *md2ms = "md2roff -ms";
-    char *md2man = "md2roff -man";
+#define MD2MS "md2roff -ms"
+#define MD2MAN "md2roff -man"
 
-    if (!strcmp(argv[1], "-ms")) {
-        argv[1] = md2ms;
-        execvp("md2ms", &argv[1]);
-    } else if (!strcmp(argv[1], "-man")) {
-        argv[1] = md2man;
-        execvp("md2man", &argv[1]);
-    }
+int main(int argc, char *argv[]) {
+
+	if (argc > 1) {
+	    if (!strcmp(argv[1], "-ms")) {
+	        argv[1] = (char *) malloc(strlen(MD2MS));
+			strcpy(argv[1], MD2MS);
+	        execvp("md2ms", &argv[1]);
+	    } else if (!strcmp(argv[1], "-man")) {
+	        argv[1] = (char *) malloc(strlen(MD2MAN));
+			strcpy(argv[1], MD2MAN);
+	        execvp("md2man", &argv[1]);
+	    }
+	}
 
     printf("Usage: %s [-ms|-man] [args]\n", argv[0]);
     return 0;
